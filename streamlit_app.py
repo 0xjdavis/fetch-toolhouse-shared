35226457bc5f45767a857ce99a94c273cd410abb
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-09-22 12:03:57 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ async def get_answer(query):
     # Define the Groq model we want to use
     #MODEL = 'mixtral-8x7b-32768'  # No result, but got code. 
     #MODEL = 'llama2-70b-4096'  # Don't use this.
-    MODEL = 'llama3-groq-70b-8192-tool-use-preview:"
+    MODEL = 'llama3-groq-70b-8192-tool-use-preview'
     # MODEL = 'gemma-7b-it' 
     
     messages = [{
